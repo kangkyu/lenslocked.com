33f7257ef97b20c0c5b05a52faa9275ba23c98c0
Jon Calhoun <joncalhoun@gmail.com>
2017-07-05 16:44:41 -0400

    Updated main.go to now use the users controller for the signup page

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 
+	"lenslocked.com/controllers"
 	"lenslocked.com/views"
 
 	"github.com/gorilla/mux"
@@ -11,7 +12,6 @@ import (
 var (
 	homeView    *views.View
 	contactView *views.View
-	signupView  *views.View
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -22,11 +22,6 @@ func home(w http.ResponseWriter, r *http.Request) {
 func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	must(contactView.Render(w, nil))
-}
-
-func signup(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(signupView.Render(w, nil))
 }
 
 // A helper function that panics on any error
@@ -41,12 +36,11 @@ func main() {
 		"views/home.gohtml")
 	contactView = views.NewView("bootstrap",
 		"views/contact.gohtml")
-	signupView = views.NewView("bootstrap",
-		"views/signup.gohtml")
+	usersC := controllers.NewUsers()
 
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
-	r.HandleFunc("/signup", signup)
+	r.HandleFunc("/signup", usersC.New)
 	http.ListenAndServe(":3000", r)
 }
