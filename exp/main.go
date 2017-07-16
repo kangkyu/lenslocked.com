eee545a90b2e6a09a86aa650955c22058ccd1511
Jon Calhoun <joncalhoun@gmail.com>
2017-07-16 19:39:57 -0400

    Completed 8.3.1

--- exp/main.go
+++ exp/main.go
@@ -1,20 +1,33 @@
 package main
 
 import (
-	"html/template"
-	"os"
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "your-password"
+	dbname   = "lenslocked_dev"
 )
 
 func main() {
-	t, err := template.ParseFiles("hello.gohtml")
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
 		panic(err)
 	}
-	data := struct {
-		Name string
-	}{"<script>alert('Howdy!');</script>"}
-	err = t.Execute(os.Stdout, data)
+	err = db.Ping()
 	if err != nil {
 		panic(err)
 	}
+
+	fmt.Println("Successfully connected!")
+	db.Close()
 }
