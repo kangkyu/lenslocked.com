7f3d410468fb7c47154657675905f65f560a06fa
Jon Calhoun <joncalhoun@gmail.com>
2017-07-16 20:53:15 -0400

    Finished 8.4.1

--- exp/main.go
+++ exp/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
 
+	"github.com/jinzhu/gorm"
 	_ "github.com/lib/pq"
 )
 
@@ -19,28 +19,9 @@ func main() {
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
 		"password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname)
-	db, err := sql.Open("postgres", psqlInfo)
+	db, err := gorm.Open("postgres", psqlInfo)
 	if err != nil {
 		panic(err)
 	}
-	var id int
-	for i := 1; i < 6; i++ {
-		// Create some fake data
-		userId := 1
-		if i > 3 {
-			userId = 2
-		}
-		amount := 1000 * i
-		description := fmt.Sprintf("USB-C Adapter x%d", i)
-		err = db.QueryRow(`
-      INSERT INTO orders (user_id, amount, description)
-      VALUES ($1, $2, $3)
-      RETURNING id`,
-			userId, amount, description).Scan(&id)
-		if err != nil {
-			panic(err)
-		}
-		fmt.Println("Created an order with the ID:", id)
-	}
-	db.Close()
+	defer db.Close()
 }
