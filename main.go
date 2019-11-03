357ea6244207a5913dbb48d5adf8e5d0f819c047
Kang-Kyu Lee <kangkyu1111@gmail.com>
2019-11-02 17:13:58 -0700

    Home and Contact 'view'

--- main.go
+++ main.go
@@ -1,23 +1,39 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+	"html/template"
 
 	"github.com/gorilla/mux"
 )
 
+var homeTemplate *template.Template
+var contactTemplate *template.Template
+
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Welcome to my awesome site</h1>")
+	if err := homeTemplate.Execute(w, nil); err != nil {
+		panic(err)
+	}
 }
 
 func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "contact us if you have any question.")
+	if err := contactTemplate.Execute(w, nil); err != nil {
+		panic(err)
+	}
 }
 
 func main() {
+	var err error
+	homeTemplate, err = template.ParseFiles("views/home.gohtml")
+	if err != nil {
+		panic(err)
+	}
+	contactTemplate, err = template.ParseFiles("views/contact.gohtml")
+	if err != nil {
+		panic(err)
+	}
 	r := mux.NewRouter()
 	r.HandleFunc("/contact/", contact)
 	r.HandleFunc("/", home)
