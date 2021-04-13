41754f957da212836a76e1097e25dd670e8d08a4
Kang-Kyu Lee <kangkyu1111@gmail.com>
2021-04-12 18:31:38 -0700

    lendslocked.com/rand

--- exp/main.go
+++ exp/main.go
@@ -3,56 +3,10 @@ package main
 import (
 	"fmt"
 
-	"lenslocked.com/models"
-
-	_ "github.com/lib/pq"
-)
-
-const (
-	host     = "localhost"
-	port     = 5432
-	user     = "postgres"
-	password = "postgres"
-	dbname   = "lenslocked_dev"
+	"lenslocked.com/rand"
 )
 
 func main() {
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
-		"password=%s dbname=%s sslmode=disable",
-		host, port, user, password, dbname)
-	us, err := models.NewUserService(psqlInfo)
-	if err != nil {
-		panic(err)
-	}
-	defer us.Close()
-	if err := us.DestructiveReset(); err != nil {
-		panic(err)
-	}
-
-	user := models.User{
-		Name: "Mike Palmer",
-		Email: "mike@dundie.com",
-	}
-	if err := us.Create(&user); err != nil {
-		panic(err)
-	}
-
-	foundUser, err := us.ByEmail("mike@dundie.com")
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(foundUser)
-
-	user.Name = "Updated Name"
-	if err := us.Update(&user); err != nil {
-		panic(err)
-	}
-
-	if err := us.Delete(foundUser.ID); err != nil {
-		panic(err)
-	}
-	_, err = us.ByID(foundUser.ID)
-	if err != models.ErrNotFound {
-		panic("user was not deleted!")
-	}
+	fmt.Println(rand.String(10))
+	fmt.Println(rand.RememberToken())
 }
